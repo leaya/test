fcca6e2da481bad57cc07e7c2a203e687c9bfa91
lihaoxin <331926815@qq.com>
2017-11-22 05:31:34 +0000

    Rebuild npm node-sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,8 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+    .copyDirectory('resources/assets/docs/css', 'public/css')
+    .copyDirectory('resources/assets/docs/js', 'public/js')
+    .copyDirectory('resources/assets/docs/fonts', 'public/fonts')
+    .copyDirectory('resources/assets/docs/img', 'public/img');
